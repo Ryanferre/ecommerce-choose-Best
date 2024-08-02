4f47046b9b32d02e321151b03a2b5c763188dba3
Ryanferre <ryandsferrer@gmail.com>
2024-08-01 21:54:31 -0300

    criação da messagem de alerta

--- validForm.js
+++ validForm.js
@@ -1,18 +1,51 @@
-function init(){
-    const InputRequisition= document.querySelector('.Register')
+import {ToFillIn} from './Processmanipulator.js'
 
-   InputRequisition.addEventListener("button-acess", function(event){
-        let InputList= document.querySelectorAll('input')
-
-        for(let i=0; i <= InputList.length; i++){
-            if(InputList[i].value == ''){
-                event.preventDefault()
-                InputList[i].style.boxShadow= '1px 1px 10px red'
-            }
-        }
-   })
-   console.log('error')
+let Form= document.querySelector('.Register')
+let Rf= document.querySelectorAll('input')
+for(let i = 0; i < Rf.length; i++){
+    Rf[i].addEventListener('click', (event)=>{
+        let ft= event.target
+        structPhase(ft.className)
+    })
 }
 
+Form.addEventListener("submit", function (event){
+    event.preventDefault()
+    let InputList= document.querySelectorAll('input')
+    const TextAlert= document.querySelector('.Atetion')
+        ToFillIn(InputList, TextAlert)
+})
 
-export {init}
+function structPhase(Phase){
+    const viewPhase = ['Email_invalido!', 'Senha_muito_curta!', 'Repita_a_senha_com_os_mesmos_caractere'];
+
+    console.log(Phase)
+    switch (Phase) {
+        case 'dt':
+            const Date= viewPhase[1].split('')
+            for(let i=0; i < Date.length; i++){
+                setTimeout(()=>{
+                    if(Date[i] == '_'){
+                        writeInform(`<p class="SpacePhase">${Date[i]}</p>`)
+                    }else{
+                        writeInform(`<p class="Informe">${Date[i]}</p>`)
+                    }
+                }, i * 100)
+            }
+            break;
+    
+        default:
+            break;
+    }
+}
+
+let arr= []
+
+
+function writeInform(dado){
+    const InformeError= document.querySelector('.Information');
+    arr.push(dado)
+    InformeError.innerHTML= arr.join('')
+    const D= document.querySelector('.SpacePhase')
+    D.style.visibility = 'hidden'
+}
