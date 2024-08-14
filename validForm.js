41393594dc9c0297ab4f2f99402f06717e282d5a
Ryanferre <ryandsferrer@gmail.com>
2024-08-13 21:07:34 -0300

    Vlidação e identificação de inputs incorretos: Pausar a cahamada do botão submmit com o metodo debounce

--- validForm.js
+++ validForm.js
@@ -1,21 +1,38 @@
 import {ToFillIn} from './Processmanipulator.js'
 
+let TimeOut;
 let Form= document.querySelector('.Register')
+let arrr= []
 
 Form.addEventListener("submit", function (event){
     event.preventDefault()
-    let InputList= document.querySelectorAll('input')
-    const TextAlert= document.querySelector('.Atetion')
-    let Name;
-    let Dado= [/\w\@gmail.com/, /\d{8}/, /\d{8}/];
+    let InputList = document.querySelectorAll('input');
+    const TextAlert = document.querySelector('.Atetion');
 
-    for(let i = 0; i < InputList.length && i < Dado.length; i++){
-        if(InputList[i].values !== Dado[i]){
-            console.log('erro')
-        }else{
-            console.log('correto')
-        }
+    const CheckdEmail= /\w\@gmail.com/
+    const CheckPasswuord= /\d{8}/
+    for (let i = 0; i < InputList.length; i++) {
+    switch (false) {
+        case CheckdEmail.test(InputList[i].value):
+            arrr.push(InputList[i].className)
+
+        case CheckPasswuord.test(InputList[i].value):
+            arrr.push(InputList[i].className)
+        default:
+            break;
     }
+    }
+
+    clearTimeout(TimeOut)
+    
+    TimeOut= setTimeout(()=>{
+        if(arrr.length >= 0){
+            structPhase()
+        }else{
+            throw new Error('erro ao exucutar a função!');
+            
+        }
+    }, 6000)
 })
 
 
@@ -24,45 +41,31 @@ Form.addEventListener("submit", function (event){
 
 
 
-function structPhase(Phase){
+function structPhase(){
     //Frases para cada error
-    const viewPhase = ['Email_invalido!', 'Senha_invalida!', 'Senha_muito_curta!', 'Caractere_diferente._Repita_novamente!', 'Campos imvalidos!'];
+    const viewPhase = `Campos_invalidos._Por_favor,_preencha_corretamente!`;
+    const BoxInform= document.querySelector('.Information')
+    BoxInform.style.marginTop= '1%'
 
     //caso o 'Phase' for um desses valores, chame a função 'Distributor' passando para
     //ela a sua perspectiva frase
     let Date;
-    switch (Phase) {
-        case 'email':
-            Date= viewPhase[0].split('')
-            Distributor(Date)
-            break;
-    
-        case 'senha':
-            Date= viewPhase[1].split('')
-            Distributor(Date)
-            break;
-        
-        case 'senhaCreate':
-            Date= viewPhase[2].split('')
-            Distributor(Date)
-            break;
-
-        case 'senhaRepeate':
-            Date= viewPhase[3].split('')
-            Distributor(Date)
-            break;
-        default:
-            break;
-    }
+   setTimeout(() => {
+    Date= viewPhase.split('')
+    Distributor(Date)
+   }, 4000);
 }
 
 
 function Distributor(dados){
     for(let i=0; i < dados.length; i++){
         setTimeout(()=>{
+            //se existir algun caracterie '_' envolva-o em um paragrafo e crie uma class diferente das demais
             if(dados[i] == '_'){
                 writeInform(`<p class="SpacePhase">${dados[i]}</p>`)
-            }else{
+            }
+            //para as demais construa um paragrafo e crie uma class do memo nome para ambas
+            else{
                 writeInform(`<p class="Informe">${dados[i]}</p>`)
             }
         }, i * 100)
@@ -72,11 +75,16 @@ let arr= []
 
 function writeInform(dado){
     const InformeError= document.querySelector('.Information');
+    const BoxInformReturn= document.querySelector('.Information')
     arr.push(dado)
     InformeError.innerHTML= arr.join('')
     let D= document.querySelectorAll('.SpacePhase')
     for(let i=0; i < D.length; i++){
         D[i].style.visibility= 'hidden'
     }
-
+    setTimeout(() => {
+         BoxInformReturn.style.marginTop= ''
+        arr.pop()
+        InformeError.innerHTML= ''
+    }, 6000);
 }
